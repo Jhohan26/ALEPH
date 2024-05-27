83c3480536422d26996a7bb57c079153008ea061
Jhohan <jhohan.reyes@uniminuto.edu.co>
2024-05-27 15:17:58 -0500

    correccion en la visualizacion de videos

--- views/recurso.php
+++ views/recurso.php
@@ -33,6 +33,7 @@ if ($verificar_inscripcion->rowCount() == 0){
 <html>
 <head>
 	<?php require_once("../inc/head.php"); ?>
+	<script type="text/javascript" src="../js/ver_videos.js" defer></script>
 </head>
 <body>
 	<header>
@@ -124,38 +125,6 @@ if ($verificar_inscripcion->rowCount() == 0){
 		</section>
 		<div class="respuesta">
 		</div>
-		<script>
-			document.addEventListener('DOMContentLoaded', function(){
-				var video = document.getElementsByTagName('video')[0];
-				var ultimo_tiempo = 0;
-				var boton = document.querySelector(".oeste");
-
-
-				video.addEventListener('timeupdate', function(){
-					// if (video.currentTime > ultimo_tiempo + 0.6){
-					// 	video.currentTime = ultimo_tiempo;
-					// }
-					// else{
-					// 	ultimo_tiempo = video.currentTime;
-					// }
-
-					if (video.currentTime > video.duration * 0.95){
-						boton.removeAttribute("disabled");
-						boton.classList.remove("desactivado");
-					}
-					else{
-						boton.classList.add("desactivado");
-						boton.setAttribute("disabled", "disabled");
-					}
-				});
-
-				// video.addEventListener('seeking', function(){
-				// 	if (video.currentTime > ultimo_tiempo){
-				// 		video.currentTime = ultimo_tiempo;
-				// 	}
-				// });
-			});
-		</script>
 	</main>
 </body>
 </html>
